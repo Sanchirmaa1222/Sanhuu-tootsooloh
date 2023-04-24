5eb0ccb0992a7185ea83a3b2de8b77c78e21fe98
sanchirmaa <sanchirma8060@gmail.com>
2023-04-24 09:39:17 +0900

    Eventlistener uusgesen

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ var uiController = (function () {})();
 var financeController = (function (){})();
 
 var appController = (function(uiController, financeController){
+  var ctrlAddItem = function(){
+  // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+  console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+  // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+  // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+  // 4. Төсвийг тооцоолно
+  // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  }
 document.querySelector('.add__btn').addEventListener("click", function(){
-  console.log('object :>> clicked');
+  ctrlAddItem();
+});
+  document.querySelector('keypress', function(event){
+    if(event.keyCode ===13 || event.which ===13){
+      ctrlAddItem();
+    }
 });
 })(uiController, financeController);
